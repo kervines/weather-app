196a767f0e265c5e4d38fc5a85aac4fcbc877511
kervines <kervinzero@gmail.com>
2024-02-08 16:46:10 -0300

    adicionado funcao de loading e usado destructuring na api

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const form = document.querySelector('#form');
 const btn = document.querySelector('#btn-search');
 const inputCity = document.querySelector('#city');
 const resultContainer = document.querySelector('.result-container');
-const apiKey = ''; //add api key
+const apiKey = '7d86fa86e8a10363930db58a41a3c7be';
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
@@ -10,19 +10,34 @@ form.addEventListener('submit', (event) => {
     fetchWeather(inputCity.value);
   }
 });
-
 async function fetchWeather(city) {
+  loading();
   const responseWeather = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
   );
   const dadosWeather = await responseWeather.json();
+  console.log(dadosWeather);
+  const {
+    main: { temp, humidity },
+    wind: { speed },
+    weather: [{ icon }],
+  } = dadosWeather;
+  console.log(icon);
+
   createResult(
     inputCity.value,
-    dadosWeather.main.temp.toFixed(1),
-    dadosWeather.main.humidity,
-    (dadosWeather.wind.speed * 3.6).toFixed(1),
-    dadosWeather.weather.icon
+    temp.toFixed(1),
+    humidity,
+    (speed * 3.6).toFixed(1),
+    icon
   );
+}
+
+function loading() {
+  resultContainer.innerHTML = `
+  <div class="loading">
+  </div>
+  `;
 }
 
 function createResult(city, temp, humidity, wind, icon) {
